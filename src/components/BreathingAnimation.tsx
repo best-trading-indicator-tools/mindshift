19d7969e900c1fc3022bbb90931aa2a90f17b7fa
Your Name <dave@best-trading-indicator.com>
2025-01-08 17:52:46 +0100

    challenges 21 days page

--- src/components/BreathingAnimation.tsx
+++ src/components/BreathingAnimation.tsx
@@ -140,6 +140,7 @@ const BreathingAnimation = forwardRef<BreathingRef, BreathingAnimationProps>(({
     }
   }, [isInitializing]);
 
+  
   // Initialize
   useEffect(() => {
     // Initialize gong sound
