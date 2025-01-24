3dbd5bb18a98aa4907ea35c37e7a023019b81ae7
Your Name <dave@best-trading-indicator.com>
2025-01-24 10:47:20 +0100

    challenge 7 jours self-hypnosis

--- src/screens/challenges-screens/ChallengesScreen.tsx
+++ src/screens/challenges-screens/ChallengesScreen.tsx
@@ -16,10 +16,10 @@ type Props = CompositeScreenProps<
 const challenges = [
   {
     id: '2',
-    title: 'Deep Mind Programming',
+    title: 'Deep Mind\nProgramming',
     duration: 7,
     description: 'Maximize your mindset transformation through strategic exercise sequencing. This 7-day challenge uses the power of self-hypnosis to amplify the effects of gratitude and affirmations.',
-    image: require('../../assets/illustrations/challenges/challenge-21.png'),
+    image: require('../../assets/illustrations/challenges/relaxation.png'),
   },
   {
     id: '1',
